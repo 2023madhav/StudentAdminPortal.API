73eecc3b168ccfe843863ddc911c6819b7642d82
2023madhav <madhavjoshi13@gmail.com>
2023-02-27 14:31:15 +0530

    Section2 commited

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using StudentAdminPortal.API.DataModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetStudentsAsync();
+    }
+}
